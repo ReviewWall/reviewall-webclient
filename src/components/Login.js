9c2ce7fd795227e132980335a8c471e6e1bb9bb8
bilgekrtgl <bilgekurtoglu06@gmail.com>
2019-11-30 23:44:17 +0300

    Axios ile post ve get işlemleri için basit örnek yapıldı.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,79 @@
 import React, {Component} from 'react';
+import axios from 'axios'
 
 class Login extends Component{
+    //bu örneği dene
+    // anasayfa gelince login e tıkla
+    // fakat gerçek server localhost adresi yazman lazım yoksa error connection hatası alırsın
 
+    state = {termAd:'' , termSoyad:'' , values:['']};
+
+    //POST*********************************************************
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        const adVeSoyad = {
+            ad: this.state.termAd,
+            soyad:this.state.termSoyad
+        }
+
+        axios.post('http://localhost:8080', adVeSoyad ).then((response) => {
+            console.log(response.data);
+        });
+    }
+
+
+    //GET*************************************************************
+    componentDidMount() {//bu fonksiyon sayfa yüklenmeden çalışır yani sayfa yüklenmeden verilerimi values arrayine atacağım
+        axios.get("http://localhost:8080").then((result) => {
+            console.log(result.data)
+            let newValues = {values: result.data};
+            this.setState(newValues);
+            //veya direkt
+            // this.setState({values : result.data}); yapabilirsin bu çalışmazsa üsttekini yap
+        });
+    //values un değerlerini çağırırsan değerler gelir ben çağırmadım render ın içinde
+
+    }
+
+
+    //Once kullanıcıdan ad ve soyad aldım formda
     render() {
-       return <div>
+       return (
+           <div id="mainDiv">
+               <h1 id="header"> AD VE SOYAD ICIN POST VE GET ISTEGI </h1>
+               <br/>
+               <form id="formId" onSubmit={event=>this.onFormSubmit(event)}>
+                   <div>
+                       <ul>
+                           <li><label>AD</label></li>
+                           <li>
+                               <input
+                                   placeholder='Adınızı giriniz..'
+                                   type="text"
+                                   value={this.state.termAd}
+                                   onChange={e => this.setState({termAd: e.target.value})}/>
+                           </li>
+                       </ul>
 
+                       <ul>
+                           <li><label>SOYAD</label></li>
+                           <li>
+                               <input
+                                   placeholder='Soyadınızı giriniz..'
+                                   type="text"
+                                   value={this.state.termSoyad}
+                                   onChange={e => this.setState({termSoyad: e.target.value})}/>
+                           </li>
+                       </ul>
 
-       </div>
+                       <button id='kaydetButton'>
+                           kaydet
+                       </button>
+                   </div>
+               </form>
+           </div>
+       );
     }
 
 }
